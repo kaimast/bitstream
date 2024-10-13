e1a9d3f3520cc11fe8dc8932ff5c7186062909d9
Kai Mast <kai@kaimast.com>
2024-10-13 00:50:02 -0700

    Port to GitHub Actions (#1)

--- include/stdbitstream.h
+++ include/stdbitstream.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <stdbitstream/unordered_set.h>
-#include <stdbitstream/set.h>
-#include <stdbitstream/list.h>
-#include <stdbitstream/string.h>
 #include <stdbitstream/array.h>
+#include <stdbitstream/list.h>
 #include <stdbitstream/map.h>
+#include <stdbitstream/set.h>
+#include <stdbitstream/string.h>
+#include <stdbitstream/unordered_set.h>
 #include <stdbitstream/vector.h>
